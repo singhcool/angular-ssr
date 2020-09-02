8678385d92e241daec8f5f5ffaf742ab4b48524a
user <rajasingh.a@contus.in>
2020-09-02 12:51:00 +0530

    Service worker removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { CookieService } from 'ngx-cookie';
 import { I18nModule } from './i18n/i18n.module';
 import { CookieModule } from 'ngx-cookie';
@@ -19,7 +17,6 @@ import { CookieModule } from 'ngx-cookie';
     I18nModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [ CookieService],
   bootstrap: [AppComponent],
